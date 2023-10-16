9a8f6cebbae2fd3de31bc713014f4adc6b29c7f6
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-10-15 21:13:56 -0300

    Initial commit

---
+++ src/test/java/br/com/josenaldo/catbaskethex/CatBasketHexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.josenaldo.catbaskethex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatBasketHexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
